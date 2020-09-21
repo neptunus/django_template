adbde792478c7b3b34889c3ca6ec5b9f3326523a
Neptúnus Egilsson <n@ptun.us>
2020-09-21 16:31:47 +0000

    added permissions to api: IsAuthenticatedOrReadOnly

--- api/views.py
+++ api/views.py
@@ -1,12 +1,13 @@
 from api.models import Post
 from api.serializers import PostSerializer
 from rest_framework import generics
-
+from rest_framework import permissions
 
 class PostList(generics.ListCreateAPIView):
     """
     List all posts, or create a new one
     """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
@@ -14,5 +15,6 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a post.
     """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
