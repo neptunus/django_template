5f4e5aed7bb249c82d9dd81a838dc6db1fe1bd43
Neptúnus Egilsson <n@ptun.us>
2020-09-21 16:28:05 +0000

    simplifying even further by using more specific generics!

--- api/views.py
+++ api/views.py
@@ -1,43 +1,18 @@
 from api.models import Post
 from api.serializers import PostSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
 
-class PostList(
-        mixins.ListModelMixin,
-        mixins.CreateModelMixin,
-        generics.GenericAPIView):
+class PostList(generics.ListCreateAPIView):
     """
     List all posts, or create a new one
     """
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-class PostDetail(
-        mixins.RetrieveModelMixin,
-        mixins.UpdateModelMixin,
-        mixins.DestroyModelMixin,
-        generics.GenericAPIView):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve, update or delete a post.
     """
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
-
